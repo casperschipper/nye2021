9c33fbdb3c50ca6780887057194d1ba9692b2ae6
Casper Schipper <casper.schipper@gmail.com>
2021-12-28 10:12:04 +0100

    ok this works not bad

--- src/Main.elm
+++ src/Main.elm
@@ -3,15 +3,21 @@ module Main exposing (Model(..), Msg(..), main, rgb, update, view)
 import Array exposing (Array)
 import Browser
 import Browser.Events
-import Html exposing (Html, div, pre, span, text)
+import Html exposing (Html, b, div, main_, pre, span, text)
 import Html.Attributes as Attr
 import Html.Events exposing (onMouseEnter)
 import Time exposing (Posix)
-import Html exposing (b)
 
 
-xRange = 50
-yRange = 25
+xRange : number
+xRange =
+    64
+
+
+yRange : number
+yRange =
+    32
+
 
 type CharElem
     = CharElem
@@ -26,10 +32,8 @@ charElem x y v =
     CharElem { x = x, y = y, value = v }
 
 
-
-
 type Model
-    = Model (Array (Array Int))
+    = Model Int (Array (Array CharElem))
 
 
 main : Program () Model Msg
@@ -40,14 +44,20 @@ main =
 init : flags -> ( Model, Cmd Msg )
 init _ =
     let
-      xss =
-        List.range 0 yRange 
-          |> List.map (\y -> 
-            List.range 0 xRange |> List.map (\x -> 
-            charAlem x y ((x + y) modBy 255)))
-        |> List.map Array.fromList |> Array.fromList             
+        xss =
+            List.range 0 yRange
+                |> List.map
+                    (\y ->
+                        List.range 0 xRange
+                            |> List.map
+                                (\x ->
+                                    charElem x y 0
+                                )
+                    )
+                |> List.map Array.fromList
+                |> Array.fromList
     in
-    ( Model xss, Cmd.none )
+    ( Model 0 xss, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
@@ -57,6 +67,7 @@ subscriptions _ =
 
 type Msg
     = OnAnimationFrame Posix
+    | OnMouseEnter Int Int
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -68,21 +79,45 @@ update msg model =
     case msg of
         OnAnimationFrame _ ->
             case model of
-                Model xss ->
-                    xss |> Array.map (Array.map updateCell) |> (\m -> ( Model m, Cmd.none ))
+                Model x xss ->
+                    case modBy 1 x of
+                        0 ->
+                            xss |> Array.map (Array.map updateCell) |> (\m -> ( Model (x + 1) m, Cmd.none ))
+
+                        _ ->
+                            ( Model (x + 1) xss, Cmd.none )
+
+        OnMouseEnter x y ->
+            ( resetCell x y model, Cmd.none )
 
 
-updateCell : Int -> Int
-updateCell i =
+updateAtIndex : (a -> a) -> Int -> Array a -> Array a
+updateAtIndex f idx array =
+    Array.get idx array
+        |> Maybe.map (\old -> Array.set idx (f old) array)
+        |> Maybe.withDefault array
+
+
+resetCell : Int -> Int -> Model -> Model
+resetCell cellX cellY (Model t xss) =
+    let
+        myF (CharElem { x, y, value }) =
+            CharElem { x = x, y = y, value = 255 }
+    in
+    Model t (updateAtIndex (\ys -> updateAtIndex myF cellX ys) cellY xss)
+
+
+updateCell : CharElem -> CharElem
+updateCell (CharElem { x, y, value }) =
     let
         next =
-            i + 1
+            value - 1
     in
-    if next > 255 then
-        0
+    if next < 0 then
+        charElem x y 0
 
     else
-        next
+        charElem x y next
 
 
 rgb : Int -> Int -> Int -> String
@@ -100,12 +135,23 @@ divideBy y x =
 
 
 gray : Int -> Html.Attribute Msg
-gray tint =
+gray x =
     let
-        x =
-            tint |> toFloat |> divideBy 255.0 |> floor
+        xf =
+            toFloat x / 255.0
+
+        tau =
+            2 * pi
+
+        w = xf * tau * 5 
+
+        ( r, g, b ) =
+            ( sin w, sin ( w + (tau / 3.0)), sin (w + (tau / 3.0)))
+
+        int255 i =
+            i * 255.0 |> floor
     in
-    Attr.style "color" (rgb x x x)
+    Attr.style "color" (rgb (int255 r) (int255 g) (int255 b))
 
 
 charXY : Int -> Int -> Html Msg
@@ -136,9 +182,55 @@ manyStars =
             )
 
 
-oneCharOfTint : Int -> Int -> Html Msg
-oneCharOfTint index tint =
-    span [ gray tint ] [ text "*" ]
+viewChar2 : CharElem -> Html Msg
+viewChar2 (CharElem { x, y, value }) =
+    let
+        xstr =
+            String.fromInt x
+
+        ystr =
+            String.fromInt y
+
+        vstr =
+            String.fromInt value
+    in
+    span [ gray value, onMouseEnter (OnMouseEnter x y) ] [ text (String.join " " [ "  -", xstr, ystr, vstr, "-  " ]) ]
+
+
+viewChar : CharElem -> Html Msg
+viewChar (CharElem { x, y, value }) =
+    span [ gray value, onMouseEnter (OnMouseEnter x y) ] [ text (charOfValue value) ]
+
+
+charOfValue : Int -> String
+charOfValue int =
+    case int // 32 of
+        0 ->
+            "`"
+
+        1 ->
+            "\\"
+
+        2 ->
+            "/"
+
+        3 ->
+            ","
+
+        4 ->
+            "-"
+
+        5 ->
+            ","
+
+        6 ->
+            ","
+
+        7 ->
+            "."
+
+        _ ->
+            "*"
 
 
 appendBr : Array (Html Msg) -> Array (Html Msg)
@@ -147,15 +239,18 @@ appendBr html =
 
 
 view : Model -> Html Msg
-view (Model xss) =
+view (Model t xss) =
     let
         stars : Array (Html Msg)
         stars =
             xss
                 |> Array.map
                     (\xs ->
-                        xs |> Array.map oneCharOfTint |> appendBr
+                        xs |> Array.map viewChar |> appendBr
                     )
                 |> Array.foldr Array.append Array.empty
     in
-    div [ Attr.style "font-family" "monospace" ] (stars |> Array.toList)
+    div []
+        [ Html.p [] [ text (String.fromInt t) ]
+        , div [ Attr.style "font-family" "monospace", Attr.style "font-size" "24px" ] (stars |> Array.toList)
+        ]
